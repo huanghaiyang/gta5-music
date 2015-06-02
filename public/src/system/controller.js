2e4324b44851737d03d45531acf8dd0dc39ac20f
huanghaiyang <huanhai24@sina.com>
2015-06-02 13:46:21 +0800

    路由bug修改

--- public/src/system/controller.js
+++ public/src/system/controller.js
@@ -1,11 +1,11 @@
 define(["app"], function(app) {
 	app.controller("AppController", ["$scope", "$location", "$route",
 		function($scope, $location, $route) {
-			$scope.module = $location.path().replace(/^\//, "");
 			/*当前路由不跳转*/
             var lastRoute = $route.current;
             $scope.$on('$locationChangeSuccess', function (event, next, nextParams) {
                 $route.current = lastRoute;
+                $scope.module = $location.path().replace(/^\//, "");
             });
 		}
 	]);
