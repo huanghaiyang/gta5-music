8e97e9a28bf37649f22ddb572d04747aa35c46cb
huanghaiyang <huanhai24@sina.com>
2015-06-02 13:30:14 +0800

    阻止当前路由跳转

--- public/src/system/controller.js
+++ public/src/system/controller.js
@@ -1,9 +1,12 @@
 define(["app"], function(app) {
 	app.controller("AppController", ["$scope", "$location", "$route",
 		function($scope, $location, $route) {
-			$scope.$on("$routeChangeSuccess", function(event, next, nextp) {
-				$scope.module = $location.path().replace(/^\//, "");
-			});
+			$scope.module = $location.path().replace(/^\//, "");
+			/*当前路由不跳转*/
+            var lastRoute = $route.current;
+            $scope.$on('$locationChangeSuccess', function (event, next, nextParams) {
+                $route.current = lastRoute;
+            });
 		}
 	]);
 });
