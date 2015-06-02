f7338e670729d6f3a4e65f8d0bc0750eaad68026
huanghaiyang <huanhai24@sina.com>
2015-06-02 17:52:34 +0800

    使用id3js解析MP3文件

--- bin/tool/af.js
+++ bin/tool/af.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var id3 = require("id3js");
 var dir = "E:/huanghaiyang/gta5-music/public/music";
 fs.readdir(dir, function(err, files) {
 	if (err) {
@@ -6,6 +7,16 @@ fs.readdir(dir, function(err, files) {
 	} else {
 		files.forEach(function(filename) {
 			var fullpath = dir + "/" + filename;
+			id3({
+				file: fullpath,
+				type: id3.OPEN_LOCAL
+			}, function(err, tags) {
+				if (err)
+					throw err;
+				else {
+
+				}
+			});
 		});
 	}
 });
