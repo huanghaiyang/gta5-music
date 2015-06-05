9212e5e97a76937b5bef265ffa26b2b16764a2c1
huanghaiyang <huanhai24@sina.com>
2015-06-05 10:03:52 +0800

    mp3文件提取图片

--- bin/tool/af.js
+++ bin/tool/af.js
@@ -8,7 +8,19 @@ fs.readdir(dir, function(err, files) {
 		files.forEach(function(filename) {
 			var fullpath = dir + "/" + filename;
 			id3.localTags(fullpath, function() {
-
+				var tags = id3.getAllTags(fullpath);
+				for (var t in tags) {
+					if (t === "picture") {
+						var dataBuffer = new Buffer(tags[t].data);
+						fs.writeFile(filename.replace(/.\w+$/, ".jpeg"), dataBuffer, function(err) {
+							if (err) {
+								throw err;
+							} else {
+								console.log("图片生成成功！");
+							}
+						});
+					}
+				}
 			}, {
 				tags: ["artist", "title", "album", "year", "comment", "track", "genre", "lyrics", "picture"]
 			});
