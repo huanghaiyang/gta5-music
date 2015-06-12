0444056b753fdc8dda3cc62495337116ff101d46
huanghaiyang <huanhai24@sina.com>
2015-06-12 13:27:12 +0800

    解决断点下载部分问题

--- controllers/file_server.js
+++ controllers/file_server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var BufferHelper = require('bufferHelper');
 var MIME = require('../lib/mine');
 var config = require('../config');
+var ObjectUtils = require('../lib/object-utils');
 
 /*资源服务器请求选项*/
 var options = {
@@ -20,6 +21,11 @@ FileServerController.prototype.get = function(req, res, next) {
 	var filename = req.params.filename;
 	options.path = "/" + encodeURIComponent(filename);
 
+	// 从文件指定位置开始传输
+	if (req.headers.range) {
+		options.headers.range = req.headers.range;
+	}
+
 	/*请求*/
 	var request = http.request(options, function(response) {
 		console.log('STATUS: ' + response.statusCode);
@@ -27,7 +33,7 @@ FileServerController.prototype.get = function(req, res, next) {
 		/*读取返回数据*/
 		var bufferHelper = new BufferHelper();
 		response.on("data", function(chunk) {
-			bufferHelper.concat(new Buffer(chunk));
+			bufferHelper.concat(chunk);
 		});
 
 		/*数据读取完成*/
@@ -39,11 +45,19 @@ FileServerController.prototype.get = function(req, res, next) {
 			if (extname && MIME[extname]) {
 				contentType = MIME[extname];
 			}
-			res.writeHead(200, {
+			var buffer = bufferHelper.toBuffer();
+			var responseHeaders = {
 				'Content-Type': contentType
-			});
+			};
+			if (options.headers.range && extname === "mp3") {
+				responseHeaders['Accept-Ranges'] = 'bytes';
+				responseHeaders['Content-Length'] = buffer.length;
+				var range = ObjectUtils.parseInt(options.headers.range.replace('bytes=', '').split(/-/));
+				responseHeaders['Content-Range:'] = 'bytes ' + range[0] + ' ' + (buffer.length + range[0]) + '/' + (buffer.length + range[0]);
+			}
+			res.writeHead(200, responseHeaders);
 			/*写返回流*/
-			res.write(bufferHelper.toBuffer());
+			res.write(buffer);
 			res.end();
 		});
 	});
--- file-server.js
+++ file-server.js
@@ -1,9 +1,14 @@
+var commander = require('commander');
+commander.option('-d --dirpath <dirpath>', 'set the music files dir path').parse(process.argv);
+var dir = commander.dirpath;
+if (!dir) {
+    throw new Error('please use -d to set the dirpath.');
+}
 var config = require('./config');
 var PORT = config.port.file_server;
 var MIME = require('./lib/mine');
-var dir = process.argv[2];
+var ObjectUtils = require('./lib/object-utils');
 var ROOT = dir ? dir : process.cwd();
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -22,16 +27,47 @@ http.createServer(function(request, response) {
 
     fs.exists(realpath, function(exists) {
         if (exists) {
-            fs.readFile(realpath, function(err, data) {
-                if (err) throw err;
+            var range = request.headers.range;
+            if (range) {
+                range = ObjectUtils.parseInt(range.replace('bytes=', '').split(/-/));
+            } else
+                range = null;
+            if (range) {
+                fs.open(realpath, 'r', function(err, fd) {
+                    if (err) throw err;
+                    var data = fs.readFileSync(realpath);
+                    if (data) {
+                        if (!range[1])
+                            range[1] = data.length;
+                    }
+                    console.log('range:' + range);
+                    var size = Math.abs(range[1] - range[0]);
+                    var bf = new Buffer(size);
+                    fs.read(fd, bf, 0, size, range[0] , function(err, bytesRead, buffer) {
+                        if (err) throw err;
 
-                response.writeHead(200, {
-                    'Content-Type': contentType
+                        response.writeHead(200, {
+                            'Content-Type': contentType
+                        });
+                        console.log('bytesRead:' + bytesRead);
+                        console.log('data length:' + buffer.length);
+                        response.write(buffer);
+                        response.end();
+                    });
                 });
-                console.log(data.length);
-                response.write(data);
-                response.end();
-            });
+
+            } else {
+                fs.readFile(realpath, function(err, data) {
+                    if (err) throw err;
+
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    console.log('data length:' + data.length);
+                    response.write(data);
+                    response.end();
+                });
+            }
         } else {
             response.writeHead(404, {
                 'Content-Type': 'text/plain'
