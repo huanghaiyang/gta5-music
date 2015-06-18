cc6bfcc64e3b094b45029c7cc7be04976a058802
huanghaiyang <huanhai24@sina.com>
2015-06-18 15:07:02 +0800

    实现随机查询

--- controllers/app/music.js
+++ controllers/app/music.js
@@ -1,54 +1,130 @@
 var Music = require('../../models/music');
 var assert = require('assert');
+var Promise = require('bluebird');
+var async = require("async");
+var ObjectUtils = require('../../lib/object-utils');
 
 function MusicController() {};
 
-MusicController.prototype.random = function(req, res, next) {
-	var number = req.query.number ? parseInt(req.query.number) : 10;
-	var yepo = {};
-	var op = "";
-	if (Math.random() >= 0.5) {
-		op = "$gte";
-		yepo[op] = "$lte";
-	} else {
-		op = "$lte";
-		yepo[op] = "$gte";
-	}
+/*随机查询一条记录*/
 
-	var random = Math.random();
-	var data = [];
-	var ran = {};
-	ran[op] = {};
-	ran[yepo[op]] = {};
-	ran[op][op] = random;
-	ran[yepo[op]][yepo[op]] = random;
+function randomOne(next) {
+	return new Promise(function(resolve, reject) {
+		var yepo = {};
+		var op = "";
+		if (Math.random() >= 0.5) {
+			op = "$gte";
+			yepo[op] = "$lte";
+		} else {
+			op = "$lte";
+			yepo[op] = "$gte";
+		}
 
-	Music.find({
-		rand: ran[op]
-	}).limit(number).exec(function(err, docs) {
-		assert.equal(err, null);
-		console.log('random ' + op + ' query success ' + 'with ' + docs.length + ' docs');
-		if (docs) {
-			if (docs.length < number) {
-				data.push(docs);
+		var random = Math.random();
+		var ran = {};
+		ran[op] = {};
+		ran[yepo[op]] = {};
+		ran[op][op] = random;
+		ran[yepo[op]][yepo[op]] = random;
 
-				Music.find({
-					rand: ran[yepo[op]]
-				}).limit(number - docs.length).exec(function(err, docs) {
-					assert.equal(err, null);
-					console.log('random ' + yepo[op] + ' query success' + 'with ' + docs.length + ' docs');
-					if (docs)
-						data.push(docs);
-					res.send(data);
-					res.end();
-				});
-			} else {
-				data = docs;
-				res.send(data);
-				res.end();
-			}
+		try {
+			Music.find({
+				rand: ran[op]
+			}).limit(1).select('name title artist year path imgPath album').exec(function(err, docs) {
+				assert.equal(err, null);
+				console.log('random ' + op + ' query success ' + 'with ' + docs.length + ' docs');
+				if (docs) {
+					if (docs.length < 1) {
+						Music.find({
+							rand: ran[yepo[op]]
+						}).limit(1).exec(function(err, docs) {
+							assert.equal(err, null);
+							console.log('random ' + yepo[op] + ' query success' + 'with ' + docs.length + ' docs');
+							resolve(docs);
+						});
+					} else {
+						resolve(docs);
+					}
+				}
+			});
+		} catch (e) {
+			reject(e);
 		}
 	});
+};
+
+/*随机查询一条记录并返回*/
+MusicController.prototype.randomOne = function(req, res, next) {
+	randomOne().then(function(data) {
+		if (data && data.length === 1) {
+			data = data[0];
+			res.send(data);
+		} else {
+			res.send(null);
+		}
+		res.end();
+	}).error(function(err) {
+		console.log(err);
+		throw err;
+	});
+};
+
+function random(number) {
+	return new Promise(function(resolve, reject) {
+		var arr = [];
+		async.forever(function(next) {
+			async.mapLimit((function() {
+				var iterator = [];
+				for (var i = 0; i < number - arr.length; i++)
+					iterator.push(i);
+				return iterator;
+			})(), 1, function(i, callback) {
+				randomOne().then(function(data) {
+					if (data && data.length === 1) {
+						data = data[0];
+						var flag = false;
+						if (arr.length === 0)
+							arr.push(data);
+						else {
+							var flag = true;
+							for (var j in arr) {
+								if (arr[j].id === data.id) {
+									flag = false;
+									break;
+								}
+							}
+							if (flag)
+								arr.push(data);
+						}
+					}
+					callback();
+				}).error(function(err) {
+					console.log(err);
+					callback();
+					reject(err);
+					throw err;
+				});
+			}, function() {
+				if (arr.length < number) {
+					next();
+				} else {
+					resolve(arr.sort(ObjectUtils.randomSort)));
+				}
+			});
+		});
+	});
+};
+
+/*随机查询多条记录并返回*/
+MusicController.prototype.random = function(req, res, next) {
+	var number = req.query.number ? parseInt(req.query.number) : 10;
+	random(number).then(function(data) {
+		res.send(data);
+		res.end();
+	}).error(function(err) {
+		console.log(err);
+		throw err;
+	});;
 };
 
 
--- routes/app/music.js
+++ routes/app/music.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var MusicController = require('../../controllers/app/music');
 var musicController = new MusicController();
 
-/* GET home page. */
+/*get some random musics*/
 router.get('/random', musicController.random);
+
+/*get a random music*/
+router.get('/randomOne', musicController.randomOne);
 
 module.exports = router;
