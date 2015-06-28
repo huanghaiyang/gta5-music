4089a0a70196292d545eace6e11c0ac906095ede
huanghaiyang <huanhai24@sina.com>
2015-06-28 15:10:56 +0800

    稳定版0.1

--- public/src/app/circle.js
+++ public/src/app/circle.js
@@ -4,144 +4,149 @@ define(['async'], function(async) {
 		jQuery.fn.circle = function(options) {
 			var defaults = {};
 			var $opts = $.extend({}, defaults, options);
+
+			function RotateController(id, $o) {
+				this.id = id;
+				this.interval = null;
+				this.$o = $o;
+				this.status = RotateController.prototype.status.stopped;
+			};
+			RotateController.prototype.status = {
+				stopped: 0,
+				rotating: 1
+			};
+
+			RotateController.prototype.rotate = function() {
+				this.status = RotateController.prototype.status.rotating;
+				var $o = this.$o;
+				this.interval = setInterval(function() {
+					if (!$o.is(":animated")) {
+						var rotated = $o.css('rotate');
+						$o.animate({
+							rotate: "360deg"
+						}, (function() {
+							if (rotated === 0 || rotated === "")
+								return 5000;
+							else {
+								return 5000 * (360 - parseInt(rotated)) / 360;
+							}
+						})(), 'linear');
+					}
+				}, (this.interval === null ? 0 : 100));
+			};
+
+			RotateController.prototype.stop = function() {
+				if (this.interval)
+					clearInterval(this.interval);
+				this.interval = null;
+				this.$o.stop();
+				this.status = RotateController.prototype.status.stopped;
+			};
+
+			function RotateControllerCollection() {
+				this.collection = {};
+			};
+			RotateControllerCollection.prototype.add = function(rotateController) {
+				this.collection[rotateController.id] = rotateController;
+			};
+			RotateControllerCollection.prototype.get = function(id) {
+				return this.collection[id];
+			};
+			RotateControllerCollection.prototype.remove = function(id) {
+				this.collection[id] = null;
+				delete this.collection[id];
+			};
+			RotateControllerCollection.prototype.removeAll = function() {
+				this.collection = {};
+			};
+
+			function SoundInstance(id, sound) {
+				this.id = id;
+				this.sound = sound;
+				this.status = SoundInstance.prototype.status.paused;
+			};
+			SoundInstance.prototype.play = function(props) {
+				this.sound.play(props);
+				this.status = SoundInstance.prototype.status.playing;
+			};
+			SoundInstance.prototype.resume = function() {
+				this.sound._resume();
+				this.status = SoundInstance.prototype.status.playing;
+			};
+			SoundInstance.prototype.pause = function() {
+				this.sound._pause();
+				this.status = SoundInstance.prototype.status.paused;
+			};
+			SoundInstance.prototype.status = {
+				playing: 0,
+				paused: 1
+			};
+
+			function SoundCollection() {
+				this.collection = {};
+			};
+			SoundCollection.prototype.add = function(soundInstance) {
+				this.collection[soundInstance.id] = soundInstance;
+			};
+			SoundCollection.prototype.get = function(id) {
+				return this.collection[id];
+			};
+			SoundCollection.prototype.remove = function(id) {
+				this.collection[id] = null;
+				delete this.collection[id];
+			};
+			SoundCollection.prototype.removeAll = function() {
+				this.collection = {};
+			};
 			return this.each(function(index, t) {
 				var $u = $(this);
 				var refreshButton;
-				//旋转
-
-				function RotateController(id, $o) {
-					this.id = id;
-					this.interval = null;
-					this.$o = $o;
-					this.status = RotateController.prototype.status.stopped;
-				};
-				RotateController.prototype.status = {
-					stopped: 0,
-					rotating: 1
-				};
-
-				RotateController.prototype.rotate = function() {
-					this.status = RotateController.prototype.status.rotating;
-					var $o = this.$o;
-					this.interval = setInterval(function() {
-						if (!$o.is(":animated")) {
-							var rotated = $o.css('rotate');
-							$o.animate({
-								rotate: "360deg"
-							}, (function() {
-								if (rotated === 0 || rotated === "")
-									return 5000;
-								else {
-									return 5000 * (360 - parseInt(rotated)) / 360;
-								}
-							})(), 'linear');
-						}
-					}, (this.interval === null ? 0 : 100));
-				};
-
-				RotateController.prototype.stop = function() {
-					if (this.interval)
-						clearInterval(this.interval);
-					this.interval = null;
-					this.$o.stop();
-					this.status = RotateController.prototype.status.stopped;
-				};
-
-				function RotateControllerCollection() {
-					this.collection = {};
-				};
-				RotateControllerCollection.prototype.add = function(rotateController) {
-					this.collection[rotateController.id] = rotateController;
-				};
-				RotateControllerCollection.prototype.get = function(id) {
-					return this.collection[id];
-				};
-				RotateControllerCollection.prototype.remove = function(id) {
-					this.collection[id] = null;
-					delete this.collection[id];
-				};
-				RotateControllerCollection.prototype.removeAll = function() {
-					this.collection = {};
-				};
 
 				var rotateControllerCollection = new RotateControllerCollection();
 
-				function SoundInstance(id, sound) {
-					this.id = id;
-					this.sound = sound;
-					this.status = SoundInstance.prototype.status.paused;
-				};
-				SoundInstance.prototype.play = function(props) {
-					this.sound.play(props);
-					this.status = SoundInstance.prototype.status.playing;
-				};
-				SoundInstance.prototype.resume = function() {
-					this.sound._resume();
-					this.status = SoundInstance.prototype.status.playing;
-				};
-				SoundInstance.prototype.pause = function() {
-					this.sound._pause();
-					this.status = SoundInstance.prototype.status.paused;
-				};
-				SoundInstance.prototype.status = {
-					playing: 0,
-					paused: 1
-				};
-
-				function SoundCollection() {
-					this.collection = {};
-				};
-				SoundCollection.prototype.add = function(soundInstance) {
-					this.collection[soundInstance.id] = soundInstance;
-				};
-				SoundCollection.prototype.get = function(id) {
-					return this.collection[id];
-				};
-				SoundCollection.prototype.remove = function(id) {
-					this.collection[id] = null;
-					delete this.collection[id];
-				};
-				SoundCollection.prototype.removeAll = function() {
-					this.collection = {};
-				};
-
 				var soundCollection = new SoundCollection();
+
+				//文件预加载队列
+				var queue = new createjs.LoadQueue();
+				//最大并发数
+				queue.setMaxConnections(1);
+				//安装声音插件
+				queue.installPlugin(createjs.Sound);
+
+				//检测单个文件加载进度
+				queue.on("fileprogress", handleFileProgressProxy());
+
+				queue.on("complete", handleComplete);
+
+				function handleComplete(event) {
+					var id = queue._loadedScripts[queue._loadedScripts.length - 1].id;
+					var $li = $u.find('li[data-id=' + id + ']');
+					$li.addClass('active');
+					$li.find('canvas').hide();
+					var sound = createjs.Sound.play(id);
+					var soundInstance = new SoundInstance(id, sound);
+					soundCollection.add(soundInstance);
+					rotateControllerCollection.get(id).rotate();
+					$li.trigger('instance');
+				};
+
+				function handleFileProgressProxy() {
+					var animationStartValue = 0.0;
+					return function(e) {
+						var id = e.item.id;
+						var $li = $u.find('li[data-id=' + id + ']');
+						$li.circleProgress({
+							value: e.progress,
+							animationStartValue: animationStartValue
+						});
+						animationStartValue = e.progress;
+						console.log(e.item.id + " is loaded " + e.progress);
+					};
+				};
 
 				function done(data, status) {
 
 					var $ls, len;
-
-					//文件预加载队列
-					var queue = new createjs.LoadQueue();
-					//最大并发数
-					queue.setMaxConnections(1);
-					//安装声音插件
-					queue.installPlugin(createjs.Sound);
-					//检测单个文件加载进度
-					queue.on("fileprogress", handleFileProgressProxy());
-
-					function handleFileProgressProxy() {
-						var animationStartValue = 0.0;
-						return function(e) {
-							var id = e.item.id;
-							var $li = $u.find('li[data-id=' + id + ']');
-							$li.circleProgress({
-								value: e.progress,
-								animationStartValue: animationStartValue
-							});
-							animationStartValue = e.progress;
-							if (e.progress === 1) {
-								$li.addClass('active');
-								$li.find('canvas').hide();
-								var sound = createjs.Sound.play(id);
-								var soundInstance = new SoundInstance(id, sound);
-								soundCollection.add(soundInstance);
-								rotateControllerCollection.get(id).rotate();
-								$li.trigger('instance');
-							}
-							console.log(e.item.id + " is loaded " + e.progress);
-						};
-					};
 
 					function bindAction($li) {
 
@@ -150,7 +155,7 @@ define(['async'], function(async) {
 							clickCount = 0,
 							firstPlay = true;
 
-						rotateController = new RotateController(id, $li);
+						rotateController = new RotateController(id, $li.find('img'));
 						rotateControllerCollection.add(rotateController);
 
 						$li.find('canvas').hide();
@@ -236,7 +241,7 @@ define(['async'], function(async) {
 								if (animateIndex < len - 1)
 									animateIndex++;
 								callback();
-							}, 50);
+							}, 100);
 							var $li = $(li);
 							var position = getPosition(animateIndex, r);
 							$li.animate({
@@ -361,13 +366,19 @@ define(['async'], function(async) {
 								var _width = $ls.eq(0).width();
 								var _r = _width * 2 / 2;
 								var r = $u.width() / 2 - _r;
-								async.mapLimit($ls, 1, function(li, callback) {
+								async.mapLimit((function() {
+									var arr = [];
+									for (var i = $ls.length - 1; i >= 0; i--) {
+										arr.push($($ls[i]));
+									}
+									return arr;
+								})(), 1, function(li, callback) {
 									var $li = $(li);
 									setTimeout(function() {
 										if (animateIndex < len - 1)
 											animateIndex++;
 										callback();
-									}, 50);
+									}, 100);
 									var position = getPosition(animateIndex, r);
 									$li.animate({
 										left: position.x - _r,
