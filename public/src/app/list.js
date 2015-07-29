2411e0f64bd87437bdebbc20736794fdbe259e48
huanghaiyang <huanhai24@sina.com>
2015-07-29 15:23:17 +0800

    更新

--- public/src/app/list.js
+++ public/src/app/list.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
-	var $musicList =$('#musicList') , params = ;
-	function loadList(){
-		
+$(document).ready(function() {
+	var $musicList = $('#musicList'),
+		$listCatas = $('.list-cata'),
+		params;
+	$listCatas.on('click', function() {
+		var $self = $(this);
+		$self.addClass('list-cata-acitve');
+		$listCatas.each(function(i, l) {
+			if ($(l) != $self) {
+				$(l).removeClass('list-cata-acitve');
+			}
+		});
+	});
+
+	function loadList() {
+
 	};
 });
