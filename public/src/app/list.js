e251fcb08be77f152c0fd970dc5543dc6e37629b
huanghaiyang <huanhai24@sina.com>
2015-08-03 23:26:25 +0800

    添加标签样式

--- public/src/app/list.js
+++ public/src/app/list.js
@@ -1,13 +1,56 @@
 $(document).ready(function() {
 	var $musicList = $('#musicList'),
 		$listCatas = $('.list-cata'),
-		params;
+		params, classConfig = {
+			li: {
+				active: 'list-cata-active'
+			}
+		};
+
+	function Tab($element) {
+		this.$element = $element;
+		this.status = Tab.status.unactived;
+	};
+
+	Tab.prototype = (function() {
+		return {
+			active: function() {
+				this.$element.addClass(classConfig.li.active);
+				this.status = Tab.status.actived;
+			},
+			disActive: function() {
+				this.$element.removeClass(classConfig.li.active);
+				this.status = Tab.status.unactived;
+			}
+		};
+	}());
+
+	Tab.status = {
+		actived: 0,
+		unactived: 1
+	};
+
+	var TabGroup = {
+		group: {},
+		add: function(i, tab) {
+			this.group[i] = tab;
+		}
+	};
+
+	$listCatas.each(function(i) {
+		$(this).data('index', i);
+		TabGroup.add(i, new Tab($(this)));
+	});
+
+	TabGroup.group[0].active();
+
 	$listCatas.on('click', function() {
 		var $self = $(this);
-		$self.addClass('list-cata-acitve');
 		$listCatas.each(function(i, l) {
-			if ($(l) != $self) {
-				$(l).removeClass('list-cata-acitve');
+			if ($(l).data('index') != $self.data('index')) {
+				$(l).removeClass(classConfig.li.active);
+			} else {
+				$self.addClass(classConfig.li.active);
 			}
 		});
 	});
